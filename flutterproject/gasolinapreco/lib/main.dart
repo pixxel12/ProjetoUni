dfac2ce7656b0aeea9b6b552caf68594c5d706e8
pixxel12 <78305479+pixxel12@users.noreply.github.com>
2021-11-03 21:55:26 -0300

    Starting
    Conectando o banco de dados e cirando as novas telas do Car Rental

--- flutterproject/gasolinapreco/lib/main.dart
+++ flutterproject/gasolinapreco/lib/main.dart
@@ -31,6 +31,70 @@ class _HomeState extends State<Home> {
           )
         ],
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.local_gas_station,
+                size: 140.0,
+                color: Colors.lightBlue[900],
+              ),
+              TextFormField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Valor do Alcool",
+                  labelStyle: TextStyle(color: Colors.lightBlue[900])
+                ),
+                style: TextStyle(
+                  color: Colors.lightBlue[900],
+                  fontSize: 26.0
+                ),
+              ),
+              TextFormField(
+                textAlign: TextAlign.center,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Valor do Gasolina",
+                    labelStyle: TextStyle(color: Colors.lightBlue[900])
+                ),
+                style: TextStyle(
+                    color: Colors.lightBlue[900],
+                    fontSize: 26.0
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: (){},
+                    child: Text(
+                      "Verificar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25.0
+                      ),
+                    ),
+                    color: Colors.lightBlue[900],
+                  ),
+                ),
+              ),
+              Text(
+                "Informe os Valores",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.lightBlue[900],
+                  fontSize: 26.0
+                ),
+              )
+            ],
+          )
+        ),
+      )
     );
   }
 }
